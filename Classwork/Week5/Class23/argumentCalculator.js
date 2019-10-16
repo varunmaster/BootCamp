8f0051b004c5f9c5e6280213f423ab8b98ccec30
Varun Master <varunmaster95@gmail.com>
2019-10-16 14:27:19 -0400

    added remainder option

---
+++ Classwork/Week5/Class23/argumentCalculator.js
@@ -0,0 +1,19 @@
+switch (process.argv[2]) {
+    case "add":
+        console.log(parseFloat(process.argv[3]) + parseFloat(process.argv[4]));
+        break;
+    case "subtract":
+        console.log(parseFloat(process.argv[3]) - parseFloat(process.argv[4]));
+        break;
+    case "multiply":
+        console.log(parseFloat(process.argv[3]) * parseFloat(process.argv[4]));
+        break;
+    case "divide":
+        console.log(parseFloat(process.argv[3]) / parseFloat(process.argv[4]));
+        break;
+    case "remainder":
+        console.log(parseFloat(process.argv[3]) % parseFloat(process.argv[4]));
+        break;
+    default:
+        console.log("you did not put an arithmatic function, stop breaking my code");
+}
